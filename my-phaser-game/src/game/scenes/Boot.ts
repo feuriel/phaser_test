deabc96cbd653199a0c5892424865e0db37e50b1
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-02-23 22:20:43 +0100

    with debugger and all scenes

--- my-phaser-game/src/game/scenes/Boot.ts
+++ my-phaser-game/src/game/scenes/Boot.ts
@@ -1,22 +1,27 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Boot extends Scene
-{
-    constructor ()
-    {
-        super('Boot');
+export class Boot extends Scene {
+    constructor() {
+        super("Boot");
     }
 
-    preload ()
-    {
+    preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.image('background', 'assets/bg.png');
+        this.load.image("background", "assets/bg.png");
+        this.load.image("sky", "assets/sky.png");
+        this.load.image("ground", "assets/platform.png");
+        this.load.image("star", "assets/star.png");
+        this.load.image("bomb", "assets/bomb.png");
+        this.load.spritesheet("dude", "assets/dude.png", {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
     }
 
-    create ()
-    {
-        this.scene.start('Preloader');
+    create() {
+        this.scene.start("Preloader");
     }
 }
+
--- my-phaser-game/src/game/scenes/MainMenu.ts
+++ my-phaser-game/src/game/scenes/MainMenu.ts
@@ -1,76 +1,163 @@
-import { GameObjects, Scene } from 'phaser';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { GameObjects, Scene } from "phaser";
 
-import { EventBus } from '../EventBus';
+import { EventBus } from "../EventBus";
 
-export class MainMenu extends Scene
-{
+export class MainMenu extends Scene {
     background: GameObjects.Image;
     logo: GameObjects.Image;
     title: GameObjects.Text;
     logoTween: Phaser.Tweens.Tween | null;
+    platforms: Phaser.Physics.Arcade.StaticGroup;
+    stars: Phaser.Physics.Arcade.Group;
+    bombs: Phaser.Physics.Arcade.Group;
+    player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+    score: number;
+    scoreText: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
+    constructor() {
+        super("MainMenu");
     }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+    create() {
+        this.score = 0;
 
-        this.logo = this.add.image(512, 300, 'logo').setDepth(100);
+        this.cursors = this.input.keyboard?.createCursorKeys();
+        this.add.image(400, 300, "sky");
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        //this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.player.body.setGravityY(300);
 
-        EventBus.emit('current-scene-ready', this);
+        this.bombs = this.physics.add.group();
+        this.physics.add.collider(this.bombs, this.platforms);
+        const hitBomb = (player: any, bomb) => {
+            this.physics.pause();
+
+            player.setTint(0xff0000);
+
+            player.anims.play("turn");
+
+            this.scene.start("GameOver");
+        };
+        this.physics.add.collider(
+            this.player,
+            this.bombs,
+            hitBomb,
+            undefined,
+            this
+        );
+
+        this.scoreText = this.add.text(40, 40, "score: 0", {
+            fontFamily: "Arial Black",
+            fontSize: 32,
+            color: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 8,
+            align: "center",
+        });
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.stars = this.physics.add.group({
+            key: "star",
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 },
+        });
+
+        this.stars.children.iterate(function (child) {
+            // @ts-ignore
+            return child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
+        });
+        this.physics.add.collider(this.stars, this.platforms);
+        //this.logo = this.add.image(512, 300, "logo").setDepth(500);
+        const collectStar = (_player: any, star: any) => {
+            star.disableBody(true, true);
+            this.score += 10;
+            this.scoreText.setText("score: " + this.score);
+            if (this.stars.countActive(true) === 0) {
+                this.stars.children.iterate(function (child) {
+                    // @ts-ignore
+                    return child.enableBody(true, child.x, 0, true, true);
+                });
+                // @ts-ignore
+                const x =
+                    this.player.x < 400
+                        ? Phaser.Math.Between(400, 800)
+                        : Phaser.Math.Between(0, 400);
+
+                const bomb = this.bombs.create(x, 16, "bomb");
+                bomb.setBounce(1);
+                bomb.setCollideWorldBounds(true);
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            }
+        };
+        this.physics.add.overlap(
+            this.player,
+            this.stars,
+            collectStar,
+            undefined,
+            this
+        );
+
+        EventBus.emit("current-scene-ready", this);
     }
-    
-    changeScene ()
-    {
-        if (this.logoTween)
-        {
-            this.logoTween.stop();
-            this.logoTween = null;
+
+    changeScene() {
+        this.scene.start("Game");
+    }
+
+    update() {
+        if (this.cursors?.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play("left", true);
+        } else if (this.cursors?.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play("right", true);
+        } else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play("turn");
         }
 
-        this.scene.start('Game');
-    }
-
-    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
-    {
-        if (this.logoTween)
-        {
-            if (this.logoTween.isPlaying())
-            {
-                this.logoTween.pause();
-            }
-            else
-            {
-                this.logoTween.play();
-            }
-        } 
-        else
-        {
-            this.logoTween = this.tweens.add({
-                targets: this.logo,
-                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-                yoyo: true,
-                repeat: -1,
-                onUpdate: () => {
-                    if (vueCallback)
-                    {
-                        vueCallback({
-                            x: Math.floor(this.logo.x),
-                            y: Math.floor(this.logo.y)
-                        });
-                    }
-                }
-            });
+        if (this.cursors?.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-470);
         }
     }
 }
+
