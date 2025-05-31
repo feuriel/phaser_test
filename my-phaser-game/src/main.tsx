bb46bbbb9d453227164f8f4c877fa2eadd8017e3
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-05-31 18:44:06 +0200

    updating portfolio link

--- my-phaser-game/src/main.tsx
+++ my-phaser-game/src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const copyright = `© ${new Date().getFullYear()} Gabriel Somogyi`;
+setTimeout(() => {
+    console.log(`made with ❤️ by Gabriel S.
+  https://gabriel-somogyi.vercel.app/
+  ${copyright}`);
+}, 3000);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-)
+    </React.StrictMode>
+);
+
